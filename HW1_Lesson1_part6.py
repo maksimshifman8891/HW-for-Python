51125e5a11108383f9de71fe9205abc39a87651a
maksimshifman8891 <58954688+maksimshifman8891@users.noreply.github.com>
2019-12-22 20:04:23 +0200

    one more delitting =))

--- HW1_Lesson1_part6.py
+++
@@ -1,7 +0,0 @@
-a = int(input('Введите a: '))
-b = int(input("Введите b:"))
-k = 1
-while a <= b:
-    k = k + 1
-    a = 1.1 * a
-print("k = ", k)
--- HW1_lesson1_part1.py
+++
@@ -1,8 +0,0 @@
-surname = str(input('what is your surname? '))
-age = int(input('how old are you? '))
-if age < 18:
-    print('go to school,', surname)
-else:
-    print('welcome on board, mr(s)', surname)
-
-
